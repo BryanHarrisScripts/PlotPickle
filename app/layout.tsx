306934024b56f48095ddb8f47ae08a468876743c
BryanHarris <bryan@bryanharris.ca>
2026-07-20 16:22:48 -0400

    Mount local runtime autosave bridge

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import LocalRuntimeBridge from "./local-runtime-bridge";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <LocalRuntimeBridge />
         {children}
       </body>
     </html>

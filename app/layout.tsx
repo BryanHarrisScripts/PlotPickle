ea1454fe9ca1346934ddefa37bb75a9f934cc0ec
BryanHarris <bryan@bryanharris.ca>
2026-07-21 10:47:22 -0400

    Apply current download links globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import CurrentDownloadLinks from "./current-download-links";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -42,6 +43,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <CurrentDownloadLinks />
         <a
           href="/voiceprint"
           style={{

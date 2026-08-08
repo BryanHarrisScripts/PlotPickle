3d476642641a945898fa279c03d185d7ac2de7f9
PlotPickle <bryan@bryanharris.ca>
2026-08-08 06:21:55 -0400

    mount Storyboard deep-link focus host

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import GitHubAppReleaseGuidance from "./github-app-release-guidance";
 import GraphicNovelTerminology from "./graphic-novel-terminology";
 import LearnEntryRouter from "./learn-entry-router";
 import PlanStudioRailHost from "./plan-studio-rail-host";
+import StoryboardStudioHost from "./storyboard-studio-host";
 import WorkspaceIntroHost from "./workspace-intro-host";
 import WriterFacingCollaborationLanguage from "./writer-facing-collaboration-language";
 import "./globals.css";
@@ -71,6 +72,7 @@ export default function RootLayout({
         <GitHubAppReleaseGuidance />
         <CollaborationWorkspaceRouter />
         <PlanStudioRailHost />
+        <StoryboardStudioHost />
         <WorkspaceIntroHost />
         <CurrentDownloadLinks />
       </body>

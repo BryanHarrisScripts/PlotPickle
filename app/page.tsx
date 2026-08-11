7f870e0499334e7ab98eb54671ba4c71630808a7
PlotPickle <bryan@bryanharris.ca>
2026-08-11 18:06:49 -0400

    Compose the modular workspace on the client

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { plotPickleCurriculum } from "../adapters/curriculum/current-catalog";
 import { answerFromCurriculum } from "../modules/creative-room/curriculum-guide";
 import LearnWorkspace from "../modules/learn/ui/learn-workspace";

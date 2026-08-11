f36f70b3163acc426826794a9a01e6145bcded1f
PlotPickle <bryan@bryanharris.ca>
2026-08-11 18:06:33 -0400

    Compose LEARN with the Creative Room module

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { plotPickleCurriculum } from "../adapters/curriculum/current-catalog";
+import { answerFromCurriculum } from "../modules/creative-room/curriculum-guide";
 import LearnWorkspace from "../modules/learn/ui/learn-workspace";
 
 export default function Home() {
-  return <LearnWorkspace curriculum={plotPickleCurriculum} />;
+  return <LearnWorkspace curriculum={plotPickleCurriculum} guide={answerFromCurriculum} />;
 }

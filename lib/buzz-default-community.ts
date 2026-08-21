894e5e3609ee358c516b444ca6bc673777d4481f
PlotPickle <bryan@bryanharris.ca>
2026-08-21 13:12:12 -0400

    fix(buzz): define canonical PlotPickle community defaults

---
+++ lib/buzz-default-community.ts
@@ -0,0 +1,8 @@
+export const PLOTPICKLE_BUZZ_COMMUNITY = Object.freeze({
+  name: "PlotPickle Community BBS",
+  relayUrl: "wss://plotpickleplayhouse.communities.buzz.xyz",
+  directoryUrl: "https://buzz.directory/communities/plotpickle-community-bbs-ad08e6622fce447297d2f893774d654d",
+  greatHallName: "great-hall",
+});
+
+export const DEFAULT_HUMAN_LORE_GLYPH = "ᛉ";

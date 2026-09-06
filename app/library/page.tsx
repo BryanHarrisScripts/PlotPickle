84026991d34f9b7a1b06c386cc76bd3365530600
PlotPickle <bryan@bryanharris.ca>
2026-09-06 18:04:59 -0400

    #1725 load isolated polish styles on direct Library route

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import LibraryWorkspace from "../../modules/library/ui/library-workspace";
+import "../issue-1725-polish.css";
 
 export default function LibraryPage() {
   return <LibraryWorkspace />;

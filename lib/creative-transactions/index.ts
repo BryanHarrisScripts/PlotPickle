0c2b664e8f059a817e2bfbe3cfeeb0175e64955e
PlotPickle <bryan@bryanharris.ca>
2026-09-14 09:34:45 -0400

    Export GitHub Creative Transaction adapter

--- lib/creative-transactions/index.ts
+++ lib/creative-transactions/index.ts
@@ -1,3 +1,4 @@
 export * from "./creative-transaction-contract";
 export * from "./local-creative-transaction-provider";
+export * from "./github-creative-transaction-provider";
 export * from "./creative-transaction-project-bridge";
